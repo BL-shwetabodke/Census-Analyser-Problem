0b3a9fc56dc4e5d5ee8aa89bd0bdda46ebef6e69
Vishal Patil <vishalpatil01@hotmail.com>
2020-03-05 12:20:39 +0530

    ADDED : Code To Test And Accomodate Us Census Data

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -1,7 +1,6 @@
 package censusanalyser;
 
 import com.google.gson.Gson;
-import org.apache.commons.collections.map.HashedMap;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -12,8 +11,9 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
-    Map<String,IndiaCensusDTO> censusMap;
-    List<IndiaCensusDTO> indiaCensusDTOList ;
+    Map<String, CensusDTO> censusMap;
+    List<CensusDTO> indiaCensusDTOList;
+
     public CensusAnalyser() {
         censusMap = new HashMap<>();
     }
@@ -25,9 +25,9 @@ public class CensusAnalyser {
             Iterator<IndiaCensusCSV> censusCSVIterator = csvBuilder.getCSVFileIterator(reader, IndiaCensusCSV.class);
             while (censusCSVIterator.hasNext()) {
                 IndiaCensusCSV indiaCensusCSV = censusCSVIterator.next();
-                censusMap.put(indiaCensusCSV.state, new IndiaCensusDTO(indiaCensusCSV));
+                censusMap.put(indiaCensusCSV.state, new CensusDTO(indiaCensusCSV));
             }
-           indiaCensusDTOList = censusMap.values().stream().collect(Collectors.toList());
+            indiaCensusDTOList = censusMap.values().stream().collect(Collectors.toList());
             return censusMap.size();
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
@@ -50,6 +50,7 @@ public class CensusAnalyser {
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
     }
+
     private <E> int getCount(Iterator<E> iterator) {
         Iterable<E> csvIterable = () -> iterator;
         int namOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
@@ -59,23 +60,41 @@ public class CensusAnalyser {
     public String getStateWiseSortedCensusData(String csvFilePath) {
         if (indiaCensusDTOList.size() == 0 || indiaCensusDTOList == null)
             throw new CensusAnalyserException("No Census Data", CensusAnalyserException.ExceptionType.NO_CENSUS_DATA);
-        Comparator<IndiaCensusDTO> censusCSVComparator = Comparator.comparing(census -> census.state);
+        Comparator<CensusDTO> censusCSVComparator = Comparator.comparing(census -> census.state);
         this.sort(censusCSVComparator);
         String sortedStateCensusJson = new Gson().toJson(indiaCensusDTOList);
         return sortedStateCensusJson;
     }
 
-    private void sort(Comparator<IndiaCensusDTO> censusCSVComparator) {
+    private void sort(Comparator<CensusDTO> censusCSVComparator) {
 
         for (int i = 0; i < indiaCensusDTOList.size() - 1; i++) {
             for (int j = 0; j < indiaCensusDTOList.size() - i - 1; j++) {
-                IndiaCensusDTO census1 = indiaCensusDTOList.get(j);
-                IndiaCensusDTO census2 = indiaCensusDTOList.get(j + 1);
+                CensusDTO census1 = indiaCensusDTOList.get(j);
+                CensusDTO census2 = indiaCensusDTOList.get(j + 1);
                 if (censusCSVComparator.compare(census1, census2) > 0) {
                     indiaCensusDTOList.set(j, census2);
                     indiaCensusDTOList.set(j + 1, census1);
                 }
             }
+        }
+    }
+
+    public int loadUsCensusData(String csvFilePath) {
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
+            ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
+            Iterator<UsCensusCSV> usCensusCSVIterator = csvBuilder.getCSVFileIterator(reader, UsCensusCSV.class);
+
+            while (usCensusCSVIterator.hasNext()) {
+                UsCensusCSV usCensusCSV = usCensusCSVIterator.next();
+                censusMap.put(usCensusCSV.state, new CensusDTO(usCensusCSV));
+            }
+            indiaCensusDTOList = censusMap.values().stream().collect(Collectors.toList());
+            return censusMap.size();
+
+        } catch (IOException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
     }
 }
