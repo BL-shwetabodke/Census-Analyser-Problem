d8eea715eb22198003ece4a812d7c54340d6bb4e
Vishal Patil <vishalpatil01@hotmail.com>
2020-03-05 10:46:13 +0530

    REFACTORED : Code To Use HashMap Instead List

--- src/main/java/censusanalyser/CSVBuilderException.java
+++ src/main/java/censusanalyser/CSVBuilderException.java
@@ -3,7 +3,7 @@ package censusanalyser;
 public class CSVBuilderException extends RuntimeException {
 
     enum ExceptionType {
-        UNABLE_TO_PARSE, CENSUS_FILE_PROBLEM
+        UNABLE_TO_PARSE, CENSUS_FILE_PROBLEM,NO_CENSUS_DATA
     }
 
     CSVBuilderException.ExceptionType type;
---
+++ src/main/java/censusanalyser/IndiaCensusDTO.java
@@ -0,0 +1,18 @@
+package censusanalyser;
+
+public class IndiaCensusDTO {
+    public String state;
+    public long population;
+    public long areaInSqKm;
+    public long densityPerSqKm;
+    public String stateCode;
+
+    public IndiaCensusDTO(IndiaCensusCSV indiaCensusCSV) {
+        state = indiaCensusCSV.state;
+        population = indiaCensusCSV.population;
+        areaInSqKm = indiaCensusCSV.areaInSqKm;
+        densityPerSqKm = indiaCensusCSV.densityPerSqKm;
+    }
+
+
+}
